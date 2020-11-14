a4b03d28211dbf9737cfa69d5a886135a7d93abe
Patryk Kubacki <patryk.kubacki@outlook.com>
2020-11-14 14:39:15 +0100

    Initialize frontend UI app

---
+++ microwaveqmeter/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Microwave Q-Meter
+      </header>
+    </div>
+  );
+}
+
+export default App;
